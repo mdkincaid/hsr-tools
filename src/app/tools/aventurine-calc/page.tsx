6e1570a896f47f212e119497bc98f0312927e828
Matthew Kincaid <matthew@kincaid.dev>
2024-04-17 16:52:39 -0400

    feat: add mantine for ui and begin to add desired pages

---
+++ src/app/tools/aventurine-calc/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return <h3>Hello, Aventurine Calc!</h3>
+  }
+  
---
+++ src/app/tools/tingyun-calc/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return <h3>Hello, Tingyun Calc!</h3>
+  }
+  
