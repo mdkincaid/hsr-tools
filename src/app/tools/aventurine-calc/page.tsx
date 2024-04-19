18eec899e3c0970be4287b05349d6db8b10fc16b
Matthew Kincaid <matthew@kincaid.dev>
2024-04-18 21:01:14 -0400

    feat: add little intro blurb to aven calc

--- src/app/tools/aventurine-calc/page.tsx
+++ src/app/tools/aventurine-calc/page.tsx
@@ -5,21 +5,24 @@ import getActualNumberOfMainstats from "@/utils/numberOfMainstats";
 import {
   Checkbox,
   Divider,
+  Group,
   NumberFormatter,
   NumberInput,
   Slider,
+  Space,
+  Stack,
   Text,
 } from "@mantine/core";
 import { useState } from "react";
 
 export default function Page() {
-  const [targetDEF, setTargetDEF] = useState<string | number>("");
+  const [targetDEF, setTargetDEF] = useState<string | number>(4000);
   const [lcBaseDEF, setLcBaseDEF] = useState<string | number>("");
   const [lcEffectDEF, setLcEffectDEF] = useState<string | number>("");
   const [isKnightActive, setIsKnightActive] = useState<boolean>(false);
   const [isBelobogActive, setIsBelobogActive] = useState<boolean>(false);
   const [mainstatAmt, setMainstatAmt] = useState(50);
-  const [substatValue, setSubstatValue] = useState<string | number>("");
+  const [substatValue, setSubstatValue] = useState<string | number>(4.86);
   const charBaseDEF = 655;
   const traceDEF = 0.35;
   const belobogDEF = 0.15;
@@ -49,73 +52,90 @@ export default function Page() {
   }
 
   return (
-    <div className="flex flex-col gap-5 w-2/4">
-      <NumberInput
-        label="Target DEF"
-        placeholder="Target DEF"
-        value={targetDEF}
-        onChange={setTargetDEF}
-        hideControls
-      />
-      <NumberInput
-        label="Lightcone Base DEF"
-        placeholder="Lightcone Base DEF"
-        value={lcBaseDEF}
-        onChange={setLcBaseDEF}
-        hideControls
-      />
-      <NumberInput
-        label="Lightcone Effect DEF%"
-        placeholder="Lightcone Effect DEF%"
-        value={lcEffectDEF}
-        onChange={setLcEffectDEF}
-        hideControls
-        suffix="%"
-      />
-      <Checkbox
-        labelPosition="left"
-        label="2pc Knight?"
-        checked={isKnightActive}
-        onChange={(event) => setIsKnightActive(event.currentTarget.checked)}
-      />
-      <Checkbox
-        labelPosition="left"
-        label="Belobog?"
-        checked={isBelobogActive}
-        onChange={(event) => setIsBelobogActive(event.currentTarget.checked)}
-      />
-      <Text size="sm">Number of DEF% Mainstats</Text>
-      <Slider
-        value={mainstatAmt}
-        onChange={setMainstatAmt}
-        step={25}
-        marks={marks()}
-        label={null}
-      />
-      <Divider size="xl" />
-      <NumberInput
-        label="DEF% Substat Value"
-        placeholder="DEF% Substat Value"
-        value={substatValue}
-        onChange={setSubstatValue}
-        suffix="%"
-        hideControls
-      />
-      <Divider size="xl" />
-      <div>
-        Missing DEF%:
-        <NumberFormatter
-          value={targetDEF ? getMissingDEFPercent() : ""}
-          decimalScale={2}
-          suffix="%"
-        ></NumberFormatter>
+    <div>
+      <div className="w-2/4 border rounded-xl p-3 m-auto text-xs">
+        The purpose of this calculator is to get a rough idea for how much DEF% is necessary in order to maximize Aventurine&apos;s A2 Crit Rate buff.
       </div>
-      <div>
-        Number of DEF substat rolls:
-        <NumberFormatter
-          value={getNumberOfSubstatRolls()}
-          decimalScale={2}
-        ></NumberFormatter>
+      <Space h="xs" />
+      <div className="w-2/4 border rounded-xl p-3 m-auto">
+        <Stack align="stretch" gap="lg" className="mb-4">
+          <NumberInput
+            label="Target DEF"
+            placeholder="Target DEF"
+            value={targetDEF}
+            onChange={setTargetDEF}
+            hideControls
+          />
+          <NumberInput
+            label="Lightcone Base DEF"
+            placeholder="Lightcone Base DEF"
+            value={lcBaseDEF}
+            onChange={setLcBaseDEF}
+            hideControls
+          />
+          <NumberInput
+            label="Lightcone Effect DEF%"
+            placeholder="Lightcone Effect DEF%"
+            value={lcEffectDEF}
+            onChange={setLcEffectDEF}
+            hideControls
+            suffix="%"
+          />
+          <Group>
+            <Checkbox
+              label="2pc Knight?"
+              checked={isKnightActive}
+              onChange={(event) =>
+                setIsKnightActive(event.currentTarget.checked)
+              }
+            />
+            <Checkbox
+              label="Belobog?"
+              checked={isBelobogActive}
+              onChange={(event) =>
+                setIsBelobogActive(event.currentTarget.checked)
+              }
+            />
+          </Group>
+          <div className="mb-2">
+            <Text size="sm">Number of DEF% Mainstats</Text>
+            <Slider
+              value={mainstatAmt}
+              onChange={setMainstatAmt}
+              step={25}
+              marks={marks()}
+              label={null}
+            />
+          </div>
+          <NumberInput
+            label="DEF% Substat Value"
+            placeholder="DEF% Substat Value"
+            value={substatValue}
+            onChange={setSubstatValue}
+            suffix="%"
+            hideControls
+          />
+        </Stack>
+        <Divider size="xl" />
+        <Stack className="mt-4">
+          <div>
+            Missing DEF%:
+            <NumberFormatter
+              value={targetDEF ? getMissingDEFPercent() : ""}
+              decimalScale={2}
+              prefix=" "
+              suffix="%"
+            />
+          </div>
+          <div>
+            Number of DEF substat rolls:
+            <NumberFormatter
+              value={getNumberOfSubstatRolls()}
+              decimalScale={2}
+              prefix=" "
+            />
+          </div>
+        </Stack>
       </div>
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
+import { Title } from "@mantine/core";
 import NavBar from "../NavBar/NavBar";
 
 export default function Header() {
     return (
         <header className="flex mb-2">
-            <h1>Laundry Tools</h1>
+            <Title order={1} size="h3">Laundry Tools</Title>
             <NavBar />
         </header>
     );
