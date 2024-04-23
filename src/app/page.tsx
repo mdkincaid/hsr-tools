347d04b66ae11ddeeb294a7aec9fe1fbcb7c7d05
Matthew Kincaid <matthew@kincaid.dev>
2024-04-23 00:10:15 -0400

    feat: start to put something on home page. NOT SATISFIED

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <div>
+      <p>Welcome to my site! This site serves multiple purposes.</p>
+      <p>The first and primary purpose of this site is to help me learn React, Next.js, TailwindCSS, Mantine and even some Vercel.</p>
+      <p>The second purpose of this site is to act as a compilation of various tools and data tables created for use with the hit gacha game Honkai Star Rail from HoyoVerse.</p>
+      <p>Majority of the tools and tables that will reside on this site are recreations of tools that were already made, by me, using google sheets.</p>
+    </div>
+  )
 }
