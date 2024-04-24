d2ece0d073aea07f179a05bfb5de2d47bfd9f6eb
Matthew Kincaid <matthew@kincaid.dev>
2024-04-24 18:44:56 -0400

    feat: add basic scaffolding for tingyun calc

--- src/app/tools/tingyun-calc/page.tsx
+++ src/app/tools/tingyun-calc/page.tsx
@@ -1,4 +1,21 @@
+import { Center, Space, Title } from "@mantine/core";
+
 export default function Page() {
-    return <h3>Hello, Tingyun Calc!</h3>
-  }
-  
+  return (
+    <div>
+      <div className="w-2/4 border rounded-xl p-3 m-auto text-xs">
+        <Center>
+          <Title order={2}>Tingyun Skill Buff Calculator</Title>
+        </Center>
+      </div>
+      <Space h="xs" />
+      <div className="w-2/4 border rounded-xl p-3 m-auto text-xs">
+        purpose goes here
+      </div>
+      <Space h="xs" />
+      <div className="w-2/4 border rounded-xl p-3 m-auto">
+        Calculator goes here
+      </div>
+    </div>
+  );
+}
