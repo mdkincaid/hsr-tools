910879c6cff665b359752e0027afb7216ce8cdd7
Matthew Kincaid <matthew@kincaid.dev>
2024-04-23 22:15:36 -0400

    feat: add link back to github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
       <p>The first and primary purpose of this site is to help me learn React, Next.js, TailwindCSS, Mantine and even some Vercel.</p>
       <p>The second purpose of this site is to act as a compilation of various tools and data tables created for use with the hit gacha game Honkai Star Rail from HoyoVerse.</p>
       <p>Majority of the tools and tables that will reside on this site are recreations of tools that were already made, by me, using google sheets.</p>
+      <br/>
+      <p>If you have any questions or feedback, please feel free to reach out to me via email or <a className="underline underline-offset-4" href="https://github.com/mdkincaid" target="_blank">github.</a></p>
     </div>
   )
 }
