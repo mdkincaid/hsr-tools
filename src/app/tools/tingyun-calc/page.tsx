ec7e2b88127034e31e8e71333e88481643536876
Matthew Kincaid <matthew@kincaid.dev>
2024-04-24 20:01:40 -0400

    feat: finish laying out all the necessary fields

--- src/app/tools/tingyun-calc/page.tsx
+++ src/app/tools/tingyun-calc/page.tsx
@@ -1,4 +1,12 @@
-import { Center, Space, Title } from "@mantine/core";
+import {
+  Center,
+  Divider,
+  NumberFormatter,
+  NumberInput,
+  Space,
+  Stack,
+  Title,
+} from "@mantine/core";
 
 export default function Page() {
   return (
@@ -14,7 +22,64 @@ export default function Page() {
       </div>
       <Space h="xs" />
       <div className="w-2/4 border rounded-xl p-3 m-auto">
-        Calculator goes here
+        <Stack align="stretch" gap="lg" className="mb-4">
+          <NumberInput label="Target Base ATK" hideControls />
+          <NumberInput label="Target LC Base ATK" hideControls />
+          <NumberInput label="Tingyun LC Base ATK" hideControls />
+          <NumberInput label="Tingyun Skill Trace Level" hideControls />
+          <NumberInput label="Extra Flat ATK" hideControls />
+          <NumberInput label="Extra ATK%" hideControls />
+        </Stack>
+        <Divider size="xl" />
+        <Stack className="mt-4 mb-4">
+          <div>
+            Target Total Base ATK:
+            <NumberFormatter value="10" prefix=" " suffix="" />
+          </div>
+          <div>
+            ATK% Buff from Tingyun:
+            <NumberFormatter value="10" prefix=" " suffix="%" />
+          </div>
+          <div>
+            ATK Buff from Tingyun:
+            <NumberFormatter value="10" prefix=" " suffix="" />
+          </div>
+          <div>
+            Hands Base ATK:
+            <NumberFormatter value="10" prefix=" " suffix="" />
+          </div>
+          <div>
+            Tingyun Trace ATK%:
+            <NumberFormatter value="10" prefix=" " suffix="%" />
+          </div>
+          <div>
+            Tingyun Base ATK:
+            <NumberFormatter value="10" prefix=" " suffix="" />
+          </div>
+          <div>
+            Tingyun Total Base ATK:
+            <NumberFormatter value="10" prefix=" " suffix="" />
+          </div>
+          <div>
+            Tingyun ATK% Cap:
+            <NumberFormatter value="10" prefix=" " suffix="%" />
+          </div>
+          <div>
+            Tingyun ATK Goal:
+            <NumberFormatter value="10" prefix=" " suffix="" />
+          </div>
+        </Stack>
+        <Divider size="xl" />
+        <Stack className="mt-4">
+          <div>
+            ATK Needed:
+            <NumberFormatter value="10" prefix=" " suffix="" />
+          </div>
+          <div>
+            ATK% Needed:
+            <NumberFormatter value="10" prefix=" " suffix="%" />
+          </div>
+        </Stack>
       </div>
     </div>
   );
